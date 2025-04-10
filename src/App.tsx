d10d72a2d0b074536b53a5f2df42deb4569056c2
Mai Tran Tuan Kiet <mttk2004@hotmail.com>
2025-04-10 15:01:37 +0700

    fix: Cannot read properties of undefined (reading createContext)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes } from './routes';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import { MDXProvider } from '@mdx-js/react';
--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useCallback, ReactNode } from 'react';
+import React, { createContext, useContext, useCallback, ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 
 type Language = 'vi' | 'en';
